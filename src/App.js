79899e2562ffe9fdaf9652290bc47b54f32aea72
Saurabh <pandey.saurabh4748@gmail.com>
2021-08-22 17:31:53 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,234 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import ContactComponents from "./contactComponents/ContactComponents"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    {/* HEADER */}
+     <header>
+      <div class="container">
+        <div class="nav">
+          <div class="nav1"></div>
+          <div class="nav2">
+            <div class="menu"><a href="#about">about</a></div>
+            <div class="menu"><a href="#projects">projects</a></div>
+            <div class="menu"><a href="https://creativeaces.medium.com/" target="_blank">Blogs</a></div>
+             <div class="menu"><a href="#contact">contact</a></div>
+          </div>
+         </div>
+      </div>
+    </header>
+    {/* HOME SCREEN */}
+    <div class="Mainbackground" id="home">
+      <div class="topBackground"></div>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+        <path
+          fill="#001517"
+          fillopacity="1"
+          d="M0,192L40,213.3C80,235,160,277,240,293.3C320,309,400,299,480,277.3C560,256,640,224,720,186.7C800,149,880,107,960,122.7C1040,139,1120,213,1200,218.7C1280,224,1360,160,1400,128L1440,96L1440,0L1400,0C1360,0,1280,0,1200,0C1120,0,1040,0,960,0C880,0,800,0,720,0C640,0,560,0,480,0C400,0,320,0,240,0C160,0,80,0,40,0L0,0Z"
+        ></path>
+      </svg>
+
+      <div class="container fluids">
+        <div class="row">
+          <div class="col-md-12 col-lg-6">
+            <div class="screenWords">
+              <h1>Hey, I'm Saurabh.</h1>
+              <p>
+                A Software developer. Specializing in building exceptional
+                trending applications. Feel free to take a look at my latest projects.
+              </p>
+              <div class="twoMainButton">
+                <div class="buttonMain">
+                  <a href="https://drive.google.com/file/d/1JYNUbrxCCypNKevnwwQDBx5i7QsHV98V/view?usp=sharing" target="_blank">Resume</a>
+                  <div class="hrizontalLine"></div>
+                </div>
+                <div class="buttonMain">
+                  <a href="#contact">Contact</a>
+                  <div class="hrizontalLine diff"></div>
+                </div>
+              </div>
+            </div>
+            <div class="footerMedias">
+
+          <a href="mailto:pandey.saurabh4748@gmail.com" target="__blank"
+                ><div class="footerMedia"><i class="fab fa-google"></i>
+                </div> </a>
+
+                <a href="https://tg.telepult.pro/Saurabh4748?locale=en" target="__blank"
+                ><div class="footerMedia"><i class="fab fa-telegram"></i>
+                </div> </a>
+            
+              <a href="https://www.linkedin.com/in/saurabh4748" target="__blank"
+                ><div class="footerMedia"><i class="fab fa-linkedin-in"></i>
+                </div> </a>
+           
+           
+              <a href="https://creativeaces.medium.com/" target="__blank"
+                > <div class="footerMedia"><i class="fab fa-medium"></i
+              ></div></a>
+            
+           
+              <a href="https://github.com/saurabh4748" target="__blank"
+                > <div class="footerMedia"><i class="fab fa-github-alt"></i
+              ></div></a>
+            
+          </div>
+         
+          </div>
+          <div class="col-md-12 col-lg-6">
+            <div class="screenImage">
+              <img src="./images/homescreen.png" alt="irene blog" />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+    
+      {/* ABOUT */}
+      <section id="about">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-12">
+            <div class="row">
+              <div class="col-md-6">
+                <div class="aboutMeImg">
+                  <img src="./images/about.png" alt="about me" />
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="about-me">
+                  <h5>About me</h5>
+                  <div class="aboutHorizontalLine"></div>
+                  <p>
+                    I enjoy creating things that live on the internet, whether
+                    that be websites, applications, or anything in between. My
+                    goal is to always build products that provide pixel-perfect,
+                    performant experiences.
+                  </p>
+                  <p>
+                    Currently, I am a final year student pursuing B.Tech with majors in 
+                    Computer Science and Engineering.  I am quite enthusiastic about 
+                    learning new and emerging technologies, and always looks for 
+                    something new to try out.
+                  </p>
+                  <p>
+                  I like sketching , video games and walking through the random places.
+                  </p>
+                  <p>
+                  In this portfolio, you will find my Projects, Blog, and all the things that I want to brag about.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  
+   {/* RECENT WORK */}
+   <div class="container" id="projects">
+      <div class="article">
+        <h2>Projects</h2>
+        <hr />
+        <div class="rows">
+          <div class="shop col-lg-3 col-md-4 col-sm-6">
+            <div class="shopBack">
+              <img src="./images/university_app.gif" alt="recent"/>
+              <div class="shoplebal">
+                <a href="https://github.com/saurabh4748/College_App_User" target="_blank">Read More</a>
+              </div>
+            </div>
+            <div class="shoptext">
+              <p>University App</p>
+              <h3>
+              This is an android application aimed to be a one-stop for Panjab University students.
+              </h3>
+            </div>
+          </div>
+          <div class="shop col-lg-3 col-md-4 col-sm-6">
+            <div class="shopBack">
+              <img src="./images/attendance.jpg" alt="recent" />
+              <div class="shoplebal">
+                <a href="https://github.com/saurabh4748/Attendance-with-Face-Recognition" target="_blank">Read More</a>
+              </div>
+            </div>
+            <div class="shoptext">
+              <p>Attendance System</p>
+              <h3>
+              Facial recognition with high accuracy using Webcam & record live in an excel sheet.
+              </h3>
+            </div>
+          </div>
+          <div class="shop col-lg-3 col-md-4 col-sm-6">
+            <div class="shopBack">
+              <img src="./images/goeaterys.gif" alt="recent" />
+              <div class="shoplebal">
+                <a href="https://github.com/saurabh4748/goEatery" target="_blank">Read More</a>
+              </div>
+            </div>
+            <div class="shoptext">
+              <p>Sell And Buy App</p>
+              <h3>
+                Responsive Website provides information of the best restaurants of the city.
+              </h3>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+   {/* CONTACT */}
+    <div class="contactme" id="contact">
+      <div class="contactOverlay">
+        <div class="container">
+          <div class="form">
+          <ContactComponents/>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+    {/* FOOTER */}
+    <footer>
+      <div class="container">
+        <div class="footerContainer">
+          <div class="footerNav">
+            <p><a href="#about">About</a></p>
+            <p><a href="https://creativeaces.medium.com/">blogs</a></p>
+            <p><a href="#projects">Projects</a></p>
+            <p><a href="#contact">contact</a></p>
+          </div>
+          <div class="footerMedias">
+
+          <a href="mailto:pandey.saurabh4748@gmail.com" target="__blank"
+                ><div class="footerMedia"><i class="fab fa-google"></i>
+                </div> </a>
+
+                <a href="https://tg.telepult.pro/Saurabh4748?locale=en" target="__blank"
+                ><div class="footerMedia"><i class="fab fa-telegram"></i>
+                </div> </a>
+            
+              <a href="https://www.linkedin.com/in/saurabh4748" target="__blank"
+                ><div class="footerMedia"><i class="fab fa-linkedin-in"></i>
+                </div> </a>
+           
+           
+              <a href="https://creativeaces.medium.com/" target="__blank"
+                > <div class="footerMedia"><i class="fab fa-medium"></i
+              ></div></a>
+            
+           
+              <a href="https://github.com/saurabh4748" target="__blank"
+                > <div class="footerMedia"><i class="fab fa-github-alt"></i
+              ></div></a>
+            
+          </div>
+          
+        </div>
+      </div>
+    </footer>
+    </>
   );
 }
 
---
+++ src/contactComponents/ContactComponents.js
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+import emailjs from 'emailjs-com';
+
+const Result = () =>{
+    return(
+        <p>Your message has been successfully sent. I will contact you soon.</p>
+    );
+}
+
+function ContactComponents(props){
+    const [result,showResult] = useState(false);
+    const sendEmail = (e) => {
+        e.preventDefault();
+    
+        emailjs.sendForm('service_zoatl6i', 'template_wjgxr5z', e.target, 'user_I3k2unmiIIz89G19350KO')
+          .then((result) => {
+              console.log(result.text);
+          }, (error) => {
+              console.log(error.text);
+          });
+          e.target.reset();
+          showResult(true);
+      }
+
+    return(
+        <form action="" onSubmit={sendEmail}>
+        <div class="formWord">
+          <h2>Say Hello!</h2>
+          <span>Full Name</span>
+          <br />
+          <input class="input100" type="text" name="fullName" required />
+          <br />
+          <span>Phone Number</span>
+          <br />
+          <input class="input100" type="text" name="phone" required />
+          <br />
+          <span>Enter Email</span>
+          <br />
+          <input class="input100" type="text" name="email" required />
+          <br />
+        </div>
+        <div class="formWord">
+          <span>Message</span>
+          <br />
+          <textarea name="message" required></textarea>
+          <br />
+          <button>SUBMIT</button>
+          <br />
+          <br />
+          <div class="row">{result ? <Result /> : null}</div>
+        </div>
+      </form>
+    );
+}
+
+export default ContactComponents;
